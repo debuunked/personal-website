2daa95d09bccc763bf60a92b70bc90799671fc73
debuunked <103308529+debuunked@users.noreply.github.com>
2022-04-12 17:19:48 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const contact = document.querySelector('.contact')
+const info = document.querySelector('.info')
+
+
+contact.addEventListener('click', function(){
+    alert("Contact me\nEmail: franknathan12@gmail.com\nDiscord: bamsi#3200")
+});
